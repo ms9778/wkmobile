24a7f16ae24a74bf090b59d40d4812271c35fa56
Headlezz <bjorniz@hotmail.dk>
2016-08-09 13:44:12 +0200

    ----- WORKING ----- v 1.0
    changelog:
    material design
    refactor of methods and functions

---
+++ WinkompassMobil/Models/ListModelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BLL;
+
+namespace Winkompass_Mobil.Models
+{
+    public class ListModelModel
+    {
+        TemplateList List = new TemplateList() { Journals = StorageWorker.GetAllStockJournals() };
+        TemplateModel kModel = new TemplateModel();
+    }
+}
